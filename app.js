bf6259dff099bfc9080610ca605bd05c1e17da84
g3ngi <kenzienabeel@hotmail.com>
2024-05-23 21:46:18 +0700

    improved login page UI

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
+const path = require("path");
 
 const userRoute = require("./app/routes/userRoute");
 const kanbanRoute = require("./app/routes/kanbanRoute.js");
 // const authRoute = require("./app/routes/authRoute");
 
-// Set up middleware
+// serve static files
 app.use("/public", express.static("public"));
+
+app.set('views', path.join(__dirname, '../../public/views'));
+app.set('view engine', 'ejs');
+
+// Set up middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
@@ -15,9 +21,6 @@ app.use(cookieParser());
 app.use("/users", userRoute);
 app.use("/kanban", kanbanRoute);
 
-// link domain /users
-
-// app.use("/auth", authRoute);
 
 // Start the server
 const PORT = process.env.PORT || 3000;
